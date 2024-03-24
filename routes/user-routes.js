733709827707c2fa137a503eaf0d70a2665d7e48
Sowmya Malur <smalur.github@gmail.com>
2024-03-23 23:27:34 -0700

    Add basic get routes for user and parking

--- routes/user-routes.js
+++ routes/user-routes.js
@@ -1,4 +1,12 @@
 const router = require('express').Router();
 const userController = require('../controllers/user-controller');
 
+router
+  .route('/')
+  .get(userController.index);
+
+router
+  .route("/:email")
+  .get(userController.findOne);
+
 module.exports = router;
