e9e5bb8145942f224618c5459243e4a862e5cf3c
Sowmya Malur <smalur.github@gmail.com>
2024-03-24 00:20:07 -0700

    Add logic to get the user data for the specific email and password

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,1 +1,33 @@
-const knex = require('knex')(require('../knexfile'));
+const knex = require("knex")(require("../knexfile"));
+
+/**
+ * Finds and sends parking meter data for a specific meter ID to the client.
+ *
+ * @param {*} req - The request object containing parameters.
+ * @param {*} res - The response object.
+ */
+const findOne = async (req, res) => {
+  try {
+    const userFound = await knex("users").where({
+      email: req.params.email,
+      password: req.body.password,
+    });
+
+    if (userFound.length === 0) {
+      return res.status(404).json({
+        message: `User not found. Either the email or password is incorrect.`,
+      });
+    }
+
+    const userData = userFound[0];
+    res.status(200).json(userData);
+  } catch (error) {
+    res.status(500).json({
+      message: `Unable to retrieve user data for user with email ${req.params.email}`,
+    });
+  }
+};
+
+module.exports = {
+  findOne: findOne,
+};
--- server.js
+++ server.js
@@ -4,14 +4,11 @@ require("dotenv").config(); // loads .env variables
 
 const { PORT, CLIENT_URL, API_KEY, DB_NAME, DB_PASSWORD } = process.env;
 const parkingRoutes = require('./routes/parking-routes');
-
-// test server
-// app.get("/", (req, res) => {
-//     res.send("Hello, World");
-//   });
+const userRoutes = require('./routes/user-routes');
 
 app.use(express.json()); 
-app.use('/parking', parkingRoutes);
+app.use('/api/parking', parkingRoutes);
+app.use('/api/user', userRoutes);
 
 app.listen(PORT, () => {
     console.log(`server running at http://localhost:${PORT}`);
