a0a84def3e7f75645aac1e46567406503de639f6
Sowmya Malur <smalur.github@gmail.com>
2024-03-23 23:59:39 -0700

    Add logic to get all parking meter information from json file and parking info for a specific meter id

--- controllers/parking-controller.js
+++ controllers/parking-controller.js
@@ -1,1 +1,52 @@
-const knex = require('knex')(require('../knexfile'));
+const knex = require("knex")(require("../knexfile"));
+const fs = require("fs");
+
+/**
+ * Retrieves and sends parking meter data to the client.
+ *
+ * @param {*} req - The request object.
+ * @param {*} res - The response object.
+ */
+const index = async (req, res) => {
+  try {
+    const parkingData = fs.readFileSync("./data/parking-meter-limited.json");
+    const parkingMeters = JSON.parse(parkingData);
+    res.status(200).send(parkingMeters);
+  } catch (err) {
+    res.status(400).send(`Error retrieving parking meters: ${err}`);
+  }
+};
+
+/**
+ * Finds and sends parking meter data for a specific meter ID to the client.
+ *
+ * @param {*} req - The request object containing parameters.
+ * @param {*} res - The response object.
+ */
+const findOne = async (req, res) => {
+  try {
+    const { meterId } = req.params;
+
+    const parkingData = fs.readFileSync("./data/parking-meter-limited.json");
+    const parkingMeters = JSON.parse(parkingData);
+
+    // Find the parking meter with the specified meter ID
+    const foundParking = parkingMeters.find((parking) => {
+      return parking.meterid === meterId;
+    });
+
+    // If the parking meter is found, send it as a response
+    if (foundParking) {
+      res.status(200).send(foundParking);
+    } else {
+      res.status(404).send("Meter with that id not found");
+    }
+  } catch (err) {
+    res.status(400).send("Error fetching parking information for the meterid");
+  }
+};
+
+module.exports = {
+  index: index,
+  findOne: findOne,
+};
