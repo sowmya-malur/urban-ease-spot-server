54efa9d710fada4bdb4d5ed67c835bd6c026d45d
Sowmya Malur <smalur.github@gmail.com>
2024-03-26 09:18:40 -0700

    Create migrations for  parking spots table

---
+++ migrations/02_create_parking_spots_table.js
@@ -0,0 +1,25 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+    return knex.schema
+        .createTable("parking_spots", (table) => {
+            table.increments("id").primary();
+            table.bigInteger("meter_id").notNullable();
+            table.string("location").notNullable();
+            table.string("status").notNullable();
+            table.timestamp("created_at").defaultTo(knex.fn.now());
+            table
+                .timestamp("updated_at")
+                .defaultTo(knex.raw("CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP"));
+        })
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+    return knex.schema.dropTable("parking_spots");
+};
