833b441a94ba0573f9ae135fb76c9c6cdc27d4d9
Sowmya Malur <smalur.github@gmail.com>
2024-03-28 18:32:24 -0700

    Add font files to public folder

--- seeds_data/parking-spots.js
+++ seeds_data/parking-spots.js
@@ -1,14 +1,13 @@
 const parkingMetersMasterData = require("../seeds_data/parking-meters-master.js");
 const statusOptions = ["vacant", "occupied", "broken"];
 
-const parkingSpotData = parkingMetersMasterData.map((parking)=>{
-    const newParkingSpot = {
-        meter_id: parking.meterid,
-        location: parking.geo_point_2d,
-        status: statusOptions[Math.floor(Math.random() * statusOptions.length)]
-    }
-     console.log("newParkingSpot", newParkingSpot); // TODO: del
-    return newParkingSpot;
+const parkingSpotData = parkingMetersMasterData.map((parking) => {
+  const newParkingSpot = {
+    meter_id: parking.meterid,
+    location: parking.geo_point_2d,
+    status: statusOptions[Math.floor(Math.random() * statusOptions.length)],
+  };
+  return newParkingSpot;
 });
 
 module.exports = parkingSpotData;
--- server.js
+++ server.js
@@ -1,19 +1,17 @@
 const express = require("express");
 const app = express();
-require("dotenv").config(); // loads .env variables
-const cors = require('cors');
+require("dotenv").config(); 
+const cors = require("cors");
 
-
-const { PORT, CLIENT_URL, API_KEY, DB_NAME, DB_PASSWORD } = process.env;
-const parkingRoutes = require('./routes/parking-routes');
-const userRoutes = require('./routes/user-routes');
+const { PORT, CLIENT_URL } = process.env;
+const parkingRoutes = require("./routes/parking-routes");
+const userRoutes = require("./routes/user-routes");
 
 app.use(cors());
-app.use(express.json()); 
-app.use('/api/parking', parkingRoutes);
-app.use('/api/user', userRoutes);
+app.use(express.json());
+app.use("/api/parking", parkingRoutes);
+app.use("/api/user", userRoutes);
 
 app.listen(PORT, () => {
-    console.log(`server running at http://localhost:${PORT}`);
-  });
-  
+  console.log(`server running at ${CLIENT_URL}:${PORT}`);
+});
